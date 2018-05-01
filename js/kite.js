977d41f0b6c29e74a179556364f5f4dd2b48ab8f
Nelson Gonzalez <nigm2005@gmail.com>
2018-04-30 22:29:19 -0500

    Simple flow, copy json to clipboard

--- js/kite.js
+++ js/kite.js
@@ -4,6 +4,8 @@ angular.module('kite', []).controller('mainController', ['$scope', function ($sc
 	$scope.json.name = '';
 	$scope.json.events = [];
 	$scope.json.entities = [];
+	$scope.floating = [];
+	$scope.flow = [];
 
 	var event = {
 		name: '',
@@ -54,6 +56,57 @@ angular.module('kite', []).controller('mainController', ['$scope', function ($sc
 	}
 
 	$scope.addEvent = function () {
+		function getEvent(name) {
+			var event_to_find = $scope.json.events.find(function (event) {
+				return event.name === name;
+			});
+			if (event_to_find) {
+				return angular.copy(event_to_find);
+			} else {
+				return event_to_find;
+			}
+		}
+		function getRow(event) {
+			var nextrow = [];
+			if (event.control.type === 'options') {
+				event.control.options.forEach(function (option) {
+					var next = getEvent(option.triggers);
+					if (next) {
+						nextrow.push(next);
+					}
+				});
+			}
+			return nextrow;
+		}
+		function isInRow(row, name) {
+			var exists = row.find(function (event) {
+				return event.name === name;
+			});
+			return exists != undefined;
+		}
+		function generateFlow() {
+			var flow = [];
+			var next = [getEvent('start')];
+			if (next.length > 0) { 
+				flow.push(next);
+			}
+			while (next.length > 0) {
+				var nextrow = [];
+				next.forEach(function (event) {
+					var row = getRow(event);
+					row.forEach(function (elem) {
+						if (!isInRow(nextrow, elem.name)) {
+							nextrow.push(elem);
+						}
+					});
+				});
+				if (nextrow.length > 0) {
+					flow.push(nextrow);
+				}
+				next = nextrow;
+			}
+			$scope.flow = flow;
+		}
 		if ($scope.control.type == 'entity') {
 			$scope.event.control = {
 				type: 'entity',
@@ -62,12 +115,17 @@ angular.module('kite', []).controller('mainController', ['$scope', function ($sc
 		} else if ($scope.control.type == 'options') {
 			$scope.event.control = {
 				type: 'options',
-				entity: $scope.control.options
+				options: $scope.control.options
 			}
 		}
 		$scope.json.events.push(angular.copy($scope.event));
+		generateFlow();
 		$scope.control = angular.copy(control);
 		$scope.event = angular.copy(event);
+	}
+
+	$scope.getRowClass = function (row) {
+		return 'row' + row.length;
 	}
 
 	$scope.addExample = function () {
@@ -77,6 +135,27 @@ angular.module('kite', []).controller('mainController', ['$scope', function ($sc
 	$scope.addEntity = function () {
 		$scope.json.entities.push(angular.copy($scope.entity));
 		$scope.entity = angular.copy(entity);
+	}
+
+	$scope.getFloatingEvents = function () {
+		return $scope.floating;
+	}
+
+	$scope.copyJson = function () {
+		var el = document.createElement('textarea');
+		el.value = $scope.getJson();
+		el.setAttribute('readonly', '');
+		el.style.position = 'absolute';
+		el.style.left = '-9999px';
+		document.body.appendChild(el);
+		var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
+		if (selected) {
+			document.getSelection().removeAllRanges();
+			document.getSelection().addRange(selected);
+		}
 	}
 
 	$scope.control = angular.copy(control);
